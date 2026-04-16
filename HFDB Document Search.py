2d6dc6c66a8b9ea2aff28206c1b1cdec13f9effa
andygeo06 <andygeo06@gmail.com>
2026-04-16 13:40:16 +0800

    Update HFDB Document Search.py
    Ughhhh

--- HFDB Document Search.py
+++ HFDB Document Search.py
@@ -121,7 +121,7 @@ def send_signal(user_name, user_email, dtrak_list):
 col_main, col_action = st.columns([3.5, 1], gap="small")
 
 with col_main:
-    st.title("HFDB Documents")
+    st.title("HFDB Document Dashboard")
     
     # Logic for Mobile Hint
     # We check if any checkboxes are ticked before showing the hint
