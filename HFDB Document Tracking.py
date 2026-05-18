afaaad7318aba6fea228d069c23c80ca841ae9d4
andygeo06 <andygeo06@gmail.com>
2026-05-18 17:20:07 +0800

    Update HFDB Document Tracking.py

--- HFDB Document Tracking.py
+++ HFDB Document Tracking.py
@@ -21,8 +21,8 @@ st.markdown("""
         
         /* Force the sidebar to be significantly narrower (~250px) */
         section[data-testid="stSidebar"] {
-            min-width: 169px !important;
-            max-width: 169px !important;
+            min-width: 170px !important;
+            max-width: 170px !important;
         }
         /* Transparent backgrounds ensure native Light/Dark theme compatibility */
     </style>
