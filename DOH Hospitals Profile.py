d18dcff4655d3b452ac877c0f76d9d5723bdb617
andygeo06 <andygeo06@gmail.com>
2026-06-17 09:42:56 +0800

    Update DOH Hospitals Profile.py

--- DOH Hospitals Profile.py
+++ DOH Hospitals Profile.py
@@ -70,7 +70,7 @@ def load_sheet_data():
     client = gspread.authorize(creds)
     
     # Access sheet using the URL parameter defined in your secrets file
-    sheet_url = st.secrets["GSHEETS_URL"]
+    sheet_url = st.secrets["https://docs.google.com/spreadsheets/d/1jX5bZX6V3M399a8D6vJjtx2BtNSkl8yfvcKGE_XLgco/edit?usp=sharing"]
     workbook = client.open_by_url(sheet_url)
     sheet = workbook.worksheet("DATA")
     
